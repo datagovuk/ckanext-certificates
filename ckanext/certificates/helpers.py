9ba6305594f469f1d6bf6e84e9cbb413af654a6e
Tom Mortimer-Jones <tom@morty.co.uk>
2014-11-06 21:11:56 +0000

    [#1] Handle bad json in the database for odi-certificate

--- ckanext/certificates/helpers.py
+++ ckanext/certificates/helpers.py
@@ -5,11 +5,14 @@ def has_certificate(pkg):
     Can be used in the template to determine if the package has a
     certificate. Returns a boolean.
     """
-    return 'odi-certificate' in pkg.extras
+    return 'odi-certificate' in pkg.extras and get_certificate_data(pkg) is not None
 
 def get_certificate_data(pkg):
     """
     Returns the dictionary containing information about the certificate for the
     given package
     """
-    return json.loads(pkg.extras.get('odi-certificate'))
+    try:
+        return json.loads(pkg.extras.get('odi-certificate'))
+    except ValueError:
+        return None
