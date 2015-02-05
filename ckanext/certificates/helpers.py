063f0f4efde97ddf088227910bd42002a61238c0
Tom Mortimer-Jones <tom@morty.co.uk>
2015-02-05 17:32:51 +0000

    Use API to create/update certificates

--- ckanext/certificates/helpers.py
+++ ckanext/certificates/helpers.py
@@ -1,4 +1,13 @@
 import json
+import requests
+import re
+import ckan.plugins.toolkit as toolkit
+from pylons import config
+from urlparse import urljoin
+
+CERTIFICATE_SERVER = config.get('ckanext.certificates.server', None)
+CERTIFICATE_USER = config.get('ckanext.certificates.user', None)
+CERTIFICATE_PASS = config.get('ckanext.certificates.pass', None)
 
 def has_certificate(pkg):
     """
@@ -16,3 +25,54 @@ def get_certificate_data(pkg):
         return json.loads(pkg.extras.get('odi-certificate'))
     except ValueError:
         return None
+
+def _get_request_data(pkg_name):
+    """
+    Returns a JSON string suitable for posting to the certificates API
+    """
+    site_url = config.get('ckan.site_url')
+    package_url = toolkit.url_for(controller='package',
+                                  action='read',
+                                  id=pkg_name)
+    url = urljoin(site_url, package_url)
+    data = {
+        'jurisdiction': 'GB',
+        'dataset': {
+            'documentationUrl': url
+        }
+    }
+    return json.dumps(data)
+
+def _post_request(relative_url, package_name):
+    """
+    Post a request to the certificate API
+    """
+    if not all([CERTIFICATE_SERVER, CERTIFICATE_USER, CERTIFICATE_PASS]):
+        return
+
+    url = urljoin(CERTIFICATE_SERVER, relative_url)
+    auth = requests.auth.HTTPBasicAuth(CERTIFICATE_USER, CERTIFICATE_PASS)
+    data = _get_request_data(package_name)
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, data=data, auth=auth, headers=headers)
+
+def create_certificate(pkg_dict):
+    """
+    Send request to create new certificate when a package is created
+    """
+    _post_request('/datasets', pkg_dict['name'])
+
+def update_certificate(pkg_dict):
+    """
+    Send request to update the certificate when a package is modified
+    """
+    extras = pkg_dict.get('extras', [])
+    extras = dict([(extra['key'], extra['value']) for extra in extras])
+
+    certificate = json.loads(extras.get('odi-certificate', '""'))
+    if not certificate:
+        return
+
+    dataset_id = re.findall("\d+", certificate['certificate_url'])[0]
+    relative_url = '/datasets/%s/certificates' % dataset_id
+    _post_request(relative_url, pkg_dict['name'])
--- ckanext/certificates/tests/test_helpers.py
+++ ckanext/certificates/tests/test_helpers.py
@@ -1,7 +1,9 @@
 from ckanext.certificates.helpers import has_certificate, get_certificate_data
+from ckanext.certificates.helpers import create_certificate, update_certificate
 from nose.tools import assert_equal
 from collections import namedtuple
 import json
+import mock
 
 Package = namedtuple('Package', ['extras'])
 
@@ -28,3 +30,83 @@ class TestGetCertificateData(object):
     def test_valid_json(self):
         pkg = Package({'odi-certificate': json.dumps(cert)})
         assert_equal(cert, get_certificate_data(pkg))
+
+class TestAPIBase(object):
+    def setUp(self):
+        self.patcher = mock.patch('requests.post')
+        self.post = self.patcher.start()
+
+    def tearDown(self):
+        self.patcher.stop()
+
+class TestCreateCertificate(TestAPIBase):
+    def test_post_is_made(self):
+        create_certificate({'name': 'foo'})
+        assert_equal(True, self.post.called)
+
+    def test_url(self):
+        create_certificate({'name': 'foo'})
+        args, kwargs = self.post.call_args
+        url = args[0]
+        assert_equal(True, url.startswith('http://192.168.11.11:3000'))
+        assert_equal(True, url.endswith('/datasets'))
+
+    def test_content_type(self):
+        create_certificate({'name': 'foo'})
+        args, kwargs = self.post.call_args
+        content_type = kwargs['headers']['content-type']
+        assert_equal('application/json', content_type)
+
+    def test_auth(self):
+        create_certificate({'name': 'foo'})
+        args, kwargs = self.post.call_args
+        auth = kwargs['auth']
+        assert_equal('dgu@example.com', auth.username)
+        assert_equal('DxmzWJnwKoXBSaDWyzeh', auth.password)
+
+    def test_data(self):
+        create_certificate({'name': 'foo'})
+        args, kwargs = self.post.call_args
+        data = json.loads(kwargs['data'])
+        assert_equal('GB', data['jurisdiction'])
+        assert_equal('http://test.ckan.net/dataset/foo',
+                     data['dataset']['documentationUrl'])
+
+class TestUpdateCertificate(TestAPIBase):
+    def setUp(self):
+        super(TestUpdateCertificate, self).setUp()
+        self.pkg_dict = {
+            'name': 'foo',
+            'extras': [{'key': 'odi-certificate',
+                        'value': '{"certificate_url": "foo/42/bar"}'}]
+        }
+
+    def test_no_update_if_no_certificate(self):
+        update_certificate({'name': 'foo'})
+        assert_equal(False, self.post.called)
+
+    def test_update_if_certificate(self):
+        update_certificate(self.pkg_dict)
+        assert_equal(True, self.post.called)
+
+    def test_url(self):
+        update_certificate(self.pkg_dict)
+        args, kwargs = self.post.call_args
+        url = args[0]
+        assert_equal(True, url.startswith('http://192.168.11.11:3000'))
+        assert_equal(True, url.endswith('/datasets/42/certificates'))
+
+    def test_auth(self):
+        update_certificate(self.pkg_dict)
+        args, kwargs = self.post.call_args
+        auth = kwargs['auth']
+        assert_equal('dgu@example.com', auth.username)
+        assert_equal('DxmzWJnwKoXBSaDWyzeh', auth.password)
+
+    def test_data(self):
+        update_certificate(self.pkg_dict)
+        args, kwargs = self.post.call_args
+        data = json.loads(kwargs['data'])
+        assert_equal('GB', data['jurisdiction'])
+        assert_equal('http://test.ckan.net/dataset/foo',
+                     data['dataset']['documentationUrl'])
